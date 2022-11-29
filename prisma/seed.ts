fceecce3f0f41d5ac09ec2d4ac05b7d7e3b7f955
Rifat <afim.k.40.40@gmail.com>
2022-11-29 23:04:23 +0100

    Playlist data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,14 +25,34 @@ const run = async () => {
     )
     const salt = bcrypt.genSaltSync()
     const user = await prisma.user.upsert({
-    where: { email: 'user@test.com' }, 
+    where: { email: 'user@test.com' },
     update: {},
     create: {
       email: 'user@test.com',
       password: bcrypt.hashSync('password', salt),
-      
+
     },
   })
+
+  const songs = await prisma.song.findMany({})
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i )=> {
+      return prisma.playlist.create({
+        data:{
+          name : `playlist #${i+1}`,
+          user : {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id
+            })),
+          },
+        }
+      })
+    
+  })
+  )
 } 
 
 run()
